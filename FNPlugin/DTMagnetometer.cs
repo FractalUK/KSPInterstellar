a7393b70df625b533e5404b9ec7e7f5471f44c36
FractalUK <philcuk@gmail.com>
2013-10-01 14:38:51 +0100

    magnetometer animation

--- FNPlugin/DTMagnetometer.cs
+++ FNPlugin/DTMagnetometer.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace FNPlugin {
     class DTMagnetometer : PartModule {
+		[KSPField(isPersistant = true)]
+		bool IsEnabled;
+		[KSPField(isPersistant = false)]
+		public string animName;
         [KSPField(isPersistant = false, guiActive = true, guiName = "|B|")]
         public string Bmag;
         [KSPField(isPersistant = false, guiActive = true, guiName = "B_r")]
@@ -15,17 +20,54 @@ namespace FNPlugin {
         [KSPField(isPersistant = false, guiActive = true, guiName = "Antimatter Flux")]
         public string ParticleFlux;
 
+
         private bool init = false;
+		protected Animation anim;
+
+		[KSPEvent(guiActive = true, guiName = "Activate Magnetometer", active = true)]
+		public void ActivateMagnetometer() {
+			anim [animName].speed = 1f;
+			anim [animName].normalizedTime = 0f;
+			anim.Blend (animName, 2f);
+			IsEnabled = true;
+		}
+
+		[KSPEvent(guiActive = true, guiName = "Deactivate Magnetometer", active = false)]
+		public void DeactivateMagnetometer() {
+			anim [animName].speed = -1f;
+			anim [animName].normalizedTime = 1f;
+			anim.Blend (animName, 2f);
+			IsEnabled = false;
+		}
 
         public override void OnStart(PartModule.StartState state) {
             if (state == StartState.Editor) { return; }
             this.part.force_activate();
 
+			anim = part.FindModelAnimators (animName).FirstOrDefault ();
+			if (anim != null) {
+				anim [animName].layer = 1;
+				if (!IsEnabled) {
+					anim [animName].normalizedTime = 1f;
+					anim [animName].speed = -1f;
 
+				} else {
+					anim [animName].normalizedTime = 0f;
+					anim [animName].speed = 1f;
+
+				}
+				anim.Play ();
+			}
         }
 
         public override void OnUpdate() {
-            
+			Events["ActivateMagnetometer"].active = !IsEnabled;
+			Events["DeactivateMagnetometer"].active = IsEnabled;
+			Fields["Bmag"].guiActive = IsEnabled;
+			Fields["Brad"].guiActive = IsEnabled;
+			Fields["Bthe"].guiActive = IsEnabled;
+			Fields["ParticleFlux"].guiActive = IsEnabled;
+
             float lat = (float)vessel.mainBody.GetLatitude(this.vessel.GetWorldPos3D());
             float Bmagf = VanAllen.getBeltMagneticFieldMag(vessel.mainBody.flightGlobalsIndex, (float)vessel.altitude, lat);
             float Bradf = VanAllen.getBeltMagneticFieldRadial(vessel.mainBody.flightGlobalsIndex, (float)vessel.altitude, lat);
