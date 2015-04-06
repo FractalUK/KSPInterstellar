3bb7acba244b02a6f76cb04f68b3ba71110deb09
sswelm <sswelm@gmail.com>
2015-04-06 19:16:23 +0200

    Improve Gui Admospheric Scoop , added Hydrogen/Helium Farmig in Low Orbit

--- FNPlugin/Collectors/ISRUScoop.cs
+++ FNPlugin/Collectors/ISRUScoop.cs
@@ -25,22 +25,38 @@ namespace FNPlugin
         public float scoopair = 0;
 
         // GUI
-        [KSPField(isPersistant = false, guiActive = true, guiName = "Flow")]
+        [KSPField(isPersistant = false, guiActive = true, guiName = "Density")]
+        public string atmosphericDensity;
+        [KSPField(isPersistant = false, guiActive = true, guiName = "Collected")]
         public string resflow;
         [KSPField(isPersistant = false, guiActive = true, guiName = "Resource")]
         public string currentresourceStr;
+        [KSPField(isPersistant = false, guiActive = true, guiName = "Percentage", guiUnits = "%")]
+        public float rescourcePercentage;
+        [KSPField(isPersistant = false, guiActive = true, guiName = "Storage")]
+        public string resourceStoragename;
+        [KSPField(isPersistant = false, guiActive = true, guiName = "Power")]
+        public string recievedPower;
+        [KSPField(isPersistant = false, guiActive = true, guiName = "Trace Atmosphere")]
+        public string densityFractionOfUpperAthmosphere;
+
+
         
         // internals
         protected float resflowf = 0;
 
         [KSPEvent(guiActive = true, guiName = "Activate Scoop", active = true)]
-        public void ActivateScoop() {
+        public void ActivateScoop() 
+        {
             scoopIsEnabled = true;
+            OnUpdate();
         }
 
         [KSPEvent(guiActive = true, guiName = "Disable Scoop", active = true)]
-        public void DisableScoop() {
+        public void DisableScoop() 
+        {
             scoopIsEnabled = false;
+            OnUpdate();
         }
 
         [KSPEvent(guiActive = true, guiName = "Toggle Resource", active = true)]
@@ -48,20 +64,29 @@ namespace FNPlugin
         {
             currentresource++;
 
-            if (ORSAtmosphericResourceHandler.getAtmosphericResourceName(vessel.mainBody.flightGlobalsIndex, currentresource) == null && ORSAtmosphericResourceHandler.getAtmosphericResourceContent(vessel.mainBody.flightGlobalsIndex, currentresource) > 0 && currentresource != 0) 
+            if (ORSAtmosphericResourceHandler.getAtmosphericResourceName(vessel.mainBody.flightGlobalsIndex, currentresource) == null
+                && ORSAtmosphericResourceHandler.getAtmosphericResourceContent(vessel.mainBody.flightGlobalsIndex, currentresource) > 0
+                && currentresource != 0)
+            {
                 ToggleResource();
+            }
             
             if (currentresource >= ORSAtmosphericResourceHandler.getAtmosphericCompositionForBody(vessel.mainBody.flightGlobalsIndex).Count) 
                 currentresource = 0;
+
+            resflow = String.Empty;
+            resflowf = 0;
         }
 
         [KSPAction("Activate Scoop")]
-        public void ActivateScoopAction(KSPActionParam param) {
+        public void ActivateScoopAction(KSPActionParam param) 
+        {
             ActivateScoop();
         }
 
         [KSPAction("Disable Scoop")]
-        public void DisableScoopAction(KSPActionParam param) {
+        public void DisableScoopAction(KSPActionParam param) 
+        {
             DisableScoop();
         }
 
@@ -105,7 +130,7 @@ namespace FNPlugin
             }
 
             // verify altitude is not too low
-            if (vessel.altitude < (PluginHelper.getMaxAtmosphericAltitude(vessel.mainBody) * (1.0 + (1.0 - PluginHelper.MaxAtmosphericAltitudeMult))))
+            if (vessel.altitude < (PluginHelper.getMaxAtmosphericAltitude(vessel.mainBody) * PluginHelper.MaxAtmosphericAltitudeMult))
             {
                 ScreenMessages.PostScreenMessage("Vessel is too low for resource accumulation", 10.0f, ScreenMessageStyle.LOWER_CENTER);
                 return;
@@ -139,15 +164,17 @@ namespace FNPlugin
             Events["ActivateScoop"].active = !scoopIsEnabled;
             Events["DisableScoop"].active = scoopIsEnabled;
             Events["ToggleResource"].active = scoopIsEnabled;
+
             Fields["resflow"].guiActive = scoopIsEnabled;
             Fields["currentresourceStr"].guiActive = scoopIsEnabled;
+            Fields["resourceStoragename"].guiActive = scoopIsEnabled;
 
             double resourcePercentage = ORSAtmosphericResourceHandler.getAtmosphericResourceContent(vessel.mainBody.flightGlobalsIndex, currentresource)*100;
             string resourceDisplayName = ORSAtmosphericResourceHandler.getAtmosphericResourceDisplayName(vessel.mainBody.flightGlobalsIndex, currentresource);
             if (resourceDisplayName != null) 
                 currentresourceStr = resourceDisplayName + "(" + resourcePercentage + "%)";
             
-            resflow = resflowf.ToString("0.000000");
+            resflow = resflowf.ToString("0.000000000");
         }
 
         public override void OnFixedUpdate() 
@@ -163,44 +190,86 @@ namespace FNPlugin
 
         private void ScoopAthmosphere(double deltaTimeInSeconds, bool offlineCollecting)
         {
-            string atmospheric_resource_name = ORSAtmosphericResourceHandler.getAtmosphericResourceName(vessel.mainBody.flightGlobalsIndex, currentresource);
+            string ors_atmospheric_resource_name = ORSAtmosphericResourceHandler.getAtmosphericResourceName(vessel.mainBody.flightGlobalsIndex, currentresource);
+            string resourceDisplayName = ORSAtmosphericResourceHandler.getAtmosphericResourceDisplayName(vessel.mainBody.flightGlobalsIndex, currentresource);
 
-            if (atmospheric_resource_name == null)
+            if (ors_atmospheric_resource_name == null)
             {
                 resflowf = 0.0f;
+                recievedPower = "error";
+                densityFractionOfUpperAthmosphere = "error";
                 return;
             }
 
-            double resourcedensity = PartResourceLibrary.Instance.GetDefinition(atmospheric_resource_name).density;
-            double respcent = ORSAtmosphericResourceHandler.getAtmosphericResourceContent(vessel.mainBody.flightGlobalsIndex, currentresource);
-            //double resourcedensity = PartResourceLibrary.Instance.GetDefinition(PluginHelper.atomspheric_resources_tocollect[currentresource]).density;
-            //double respcent = PluginHelper.getAtmosphereResourceContent(vessel.mainBody.flightGlobalsIndex, currentresource);
+            // map ors resource to kspi resource
+            
+            if (PluginHelper.OrsResourceMappings == null || !PluginHelper.OrsResourceMappings.TryGetValue(ors_atmospheric_resource_name, out resourceStoragename))
+                resourceStoragename = ors_atmospheric_resource_name;
+            else
+            {
+                // if resource name does not exits, use ors atmospheric resource name
+                if (!PartResourceLibrary.Instance.resourceDefinitions.Contains(resourceStoragename))
+                    resourceStoragename = ors_atmospheric_resource_name;
+            }
 
-            double airdensity = (part.vessel.atmDensity + PluginHelper.MinAtmosphericAirDensity) / 1000.0;
+            //double resourcedensity = PartResourceLibrary.Instance.GetDefinition(PluginHelper.atomspheric_resources_tocollect[currentresource]).density;
+            double resourcedensity = PartResourceLibrary.Instance.GetDefinition(resourceStoragename).density;
+
+
+            var maxAltitudeAtmosphere = PluginHelper.getMaxAtmosphericAltitude(vessel.mainBody);
+            
+            double upperAtmospherFraction = Math.Max(0, (vessel.altitude - maxAltitudeAtmosphere) / Math.Max(0.000001, maxAltitudeAtmosphere * PluginHelper.MaxAtmosphericAltitudeMult - maxAltitudeAtmosphere));
+            double upperatmosphereDensity = 1 - upperAtmospherFraction;
+            densityFractionOfUpperAthmosphere = (upperatmosphereDensity * 100.0).ToString("0.000") + "%";
+            double airDensity = part.vessel.atmDensity + (PluginHelper.MinAtmosphericAirDensity * upperatmosphereDensity);
+            atmosphericDensity = airDensity.ToString("0.00000000");
+
+            var hydrogenTax = 0.4 * Math.Sin(upperAtmospherFraction * Math.PI * 0.5);
+            var heliumTax = 0.2 * Math.Sin(upperAtmospherFraction * Math.PI);
+
+            double rescourceFraction = (1.0 - hydrogenTax - heliumTax) * ORSAtmosphericResourceHandler.getAtmosphericResourceContent(vessel.mainBody.flightGlobalsIndex, currentresource);
+
+            // increase density hydrogen
+            if (resourceDisplayName == "Hydrogen")
+                rescourceFraction += hydrogenTax;
+            else if (resourceDisplayName == "Helium")
+                rescourceFraction += heliumTax;
+
+            rescourcePercentage = (float)rescourceFraction * 100f;
+
+            
             double powerrequirements = (scoopair / 0.15f) * 6f * (float)PluginHelper.PowerConsumptionMultiplier;
                 
             double airspeed = part.vessel.srf_velocity.magnitude + 40.0;
-            double air = airspeed * airdensity * scoopair / resourcedensity;
+            double air = airspeed * (airDensity / 1000) * scoopair / resourcedensity;
 
-            if (respcent == 0 || vessel.altitude > (PluginHelper.getMaxAtmosphericAltitude(vessel.mainBody) * PluginHelper.MaxAtmosphericAltitudeMult))
+            if (rescourceFraction <= 0 || vessel.altitude > (PluginHelper.getMaxAtmosphericAltitude(vessel.mainBody) * PluginHelper.MaxAtmosphericAltitudeMult))
             {
                 resflowf = 0.0f;
+                recievedPower = "off";
+                densityFractionOfUpperAthmosphere = "too high";
+                rescourcePercentage = 0;
                 return;
             }
+            
+            double scoopedAtm = (float)(air * rescourceFraction);
 
-            double scoopedAtm = air * respcent;
+            // calculate avialble power
             float powerreceived = Math.Max(consumeFNResource(powerrequirements * TimeWarp.fixedDeltaTime, FNResourceManager.FNRESOURCE_MEGAJOULES), 0);
             last_power_percentage = offlineCollecting ? last_power_percentage : (float)(powerreceived / powerrequirements / TimeWarp.fixedDeltaTime);
+
+            recievedPower = (last_power_percentage * powerrequirements).ToString("0.0") + " MW / " + powerrequirements.ToString("0.0") + " MW";
+
             double resourceChange = scoopedAtm * last_power_percentage * deltaTimeInSeconds;
 
             if (offlineCollecting)
             {
                 string numberformat = resourceChange > 100 ? "0" : "0.00";
-                ScreenMessages.PostScreenMessage("Atmospheric Scoop collected " + resourceChange.ToString(numberformat) + " " + atmospheric_resource_name, 10.0f, ScreenMessageStyle.LOWER_CENTER);
+                ScreenMessages.PostScreenMessage("Atmospheric Scoop collected " + resourceChange.ToString(numberformat) + " " + resourceStoragename, 10.0f, ScreenMessageStyle.LOWER_CENTER);
             }
 
             //resflowf = (float)part.RequestResource(atmospheric_resource_name, -scoopedAtm * powerpcnt * TimeWarp.fixedDeltaTime);
-            resflowf = (float)ORSHelper.fixedRequestResource(part, atmospheric_resource_name, -resourceChange);
+            resflowf = (float)ORSHelper.fixedRequestResource(part, resourceStoragename, -resourceChange);
             resflowf = -resflowf / TimeWarp.fixedDeltaTime;
         }
 
